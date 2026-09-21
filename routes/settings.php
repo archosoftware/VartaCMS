6508d3ec460e4e5d76a16e2d94b954d1340b8bc9
BijjuXD <103484184+BijjuXD@users.noreply.github.com>
2026-09-21 18:22:35 +0530

    refactor: remove single-file livewire component

--- routes/settings.php
+++ routes/settings.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Route::livewire('settings/profile', 'pages::settings.profile')->name('profile.edit');
+    Route::livewire('settings/profile', 'settings.profile')->name('profile.edit');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::livewire('settings/appearance', 'pages::settings.appearance')->name('appearance.edit');
+    Route::livewire('settings/appearance', 'settings.appearance')->name('appearance.edit');
 
-    Route::livewire('settings/security', 'pages::settings.security')
+    Route::livewire('settings/security', 'settings.security')
         ->middleware([
             'password.confirm',
         ])
